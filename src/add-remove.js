09773d5295da3b78dfe99350b829dec3a0593c6e
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-10-09 13:08:35 -0300

    Fix linter errors.

--- src/add-remove.js
+++ src/add-remove.js
@@ -1,16 +1,16 @@
-import Task from "./task";
+import Task from './task.js';
 
 export const add = (taskList, inputText, index) => {
-    const newTask = new Task(inputText, false, index);
-    taskList.push(newTask);
-  }
+  const newTask = new Task(inputText, false, index);
+  taskList.push(newTask);
+};
 
 export const remove = (taskList, index) => {
-    const idx = parseInt(index, 10);
+  const idx = parseInt(index, 10);
 
-    taskList.splice(idx, 1);
+  taskList.splice(idx, 1);
 
-    for (let i = 0; i < taskList.length; i += 1) {
-      taskList[i].index = i;
-    }
+  for (let i = 0; i < taskList.length; i += 1) {
+    taskList[i].index = i;
   }
+};
--- src/add-remove.test.js
+++ src/add-remove.test.js
@@ -1,29 +1,29 @@
-import { add, remove } from './add-remove';
-import Task from './task';
+import { add, remove } from './add-remove.js';
+import Task from './task.js';
 
 const taskList = [];
 const description = 'Test Description';
 
 describe('Add and Remove Functions', () => {
-    test('Add Function', () => {
-        //Arrange
-        const index = 0;
-        const task = new Task(description, false, index);
+  test('Add Function', () => {
+    // Arrange
+    const index = 0;
+    const task = new Task(description, false, index);
 
-        //Act
-        add(taskList, description, index);
+    // Act
+    add(taskList, description, index);
 
-        //Assert
-        expect(taskList[0]).toStrictEqual(task);
-    });
+    // Assert
+    expect(taskList[0]).toStrictEqual(task);
+  });
 
-    test('Remove Function', () => {
-        //Arrange
-        const index = 0;
+  test('Remove Function', () => {
+    // Arrange
+    const index = 0;
 
-        //Act
-        remove(taskList, index);
-        
-        expect(taskList.length).toBe(0);
-    })
+    // Act
+    remove(taskList, index);
+
+    expect(taskList.length).toBe(0);
+  });
 });
